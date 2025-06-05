ecfdb36df4039be1ad349feb8226803d86721222
wuyijun <wuyijun1129@hotmail.com>
2025-06-05 20:06:38 +0800

    fix after merge - remove duplicate method

--- android/controller/src/main/java/org/openbot/controller/ConnectionSelector.kt
+++ android/controller/src/main/java/org/openbot/controller/ConnectionSelector.kt
@@ -41,13 +41,4 @@ object ConnectionSelector {
         }
     }
 
-    private fun isWifiApEnabled(context: Context): Boolean {
-        val wifiManager = context.getSystemService(WIFI_SERVICE) as WifiManager
-        return try {
-            val method = wifiManager.javaClass.getDeclaredMethod("isWifiApEnabled")
-            method.invoke(wifiManager) as? Boolean == true
-        } catch (ignored: Throwable) {
-            false
-        }
-    }
 }
