211abdcb5d1722c2c6f3d0f2c51c7d1c668b111b
wuyijun <wuyijun1129@hotmail.com>
2025-09-01 13:24:06 +0800

    Adjust PID parameters - mark4 试图调和截至目前最佳和次佳参数组合（见之前commit），待测试

--- android/robot/src/main/java/org/openbot/tracking/RobotController.java
+++ android/robot/src/main/java/org/openbot/tracking/RobotController.java
@@ -25,9 +25,9 @@ public class RobotController {
     final double KD_A = 1.0; //0.0;
 
     // 舵机角度PID增益
-    final double KP_SA = 0.7; //0.20; //0.25;
+    final double KP_SA = 0.45; //0.20; //0.25;
     final double KI_SA = 0.0;
-    final double KD_SA = 0.05; //0.06; //0.05;
+    final double KD_SA = 0.045; //0.06; //0.05;
 
     // 创建机器人控制器实例
     PIDController robot = new PIDController(
